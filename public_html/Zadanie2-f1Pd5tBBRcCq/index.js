1bd4386a99212860f0fbcb85b316f750a2a4b54b
Gergely Tóth <102431122+GergelyToth-stuba@users.noreply.github.com>
2023-10-22 20:39:55 +0200

    Got addresses from chatgpt

---
+++ public_html/Zadanie2-f1Pd5tBBRcCq/index.js
@@ -0,0 +1,73 @@
+var data = {
+    "Slovensko": {
+        "Bratislavský kraj": [
+            "Bratislava I", "Bratislava II", "Bratislava III", "Bratislava IV", "Bratislava V", "Malacky", "Pezinok", "Senec"
+        ],
+        "Trnavský kraj": [
+            "Dunajská Streda", "Galanta", "Hlohovec", "Pezinok", "Senica", "Skalica", "Trnava"
+        ],
+        "Trenčiansky kraj": [
+            "Bánovce nad Bebravou", "Myjava", "Nové Mesto nad Váhom", "Partizánske", "Považská Bystrica", "Púchov", "Trenčín"
+        ],
+        "Žilinský kraj": [
+            "Čadca", "Dolný Kubín", "Kysucké Nové Mesto", "Liptovský Mikuláš", "Martin", "Námestovo", "Ružomberok", "Tvrdošín"
+        ],
+        "Banskobystrický kraj": [
+            "Banská Bystrica", "Brezno", "Detva", "Krupina", "Lučenec", "Poltár", "Revúca", "Rimavská Sobota", "Veľký Krtíš", "Zvolen", "Žarnovica"
+        ],
+        "Prešovský kraj": [
+            "Bardejov", "Humenné", "Kežmarok", "Levoča", "Medzilaborce", "Poprad", "Prešov", "Sabinov", "Snina", "Stará Ľubovňa", "Stropkov", "Svidník", "Vranov nad Topľou"
+        ],
+        "Košický kraj": [
+            "Gelnica", "Košice I", "Košice II", "Košice III", "Košice IV", "Košice-okolie", "Michalovce", "Rožňava", "Sobrance", "Spišská Nová Ves", "Trebišov"
+        ],
+        "Nitranský kraj": [
+            "Komárno", "Levice", "Nitra", "Nové Zámky", "Šaľa", "Topoľčany", "Zlaté Moravce"
+        ]
+    },
+    "Česko": {
+        "Hlavní město Praha": [
+            "Praha 1", "Praha 2", "Praha 3", "Praha 4", "Praha 5",
+            "Praha 6", "Praha 7", "Praha 8", "Praha 9", "Praha 10", "Praha 11", "Praha 12", "Praha 13", "Praha 14", "Praha 15"
+        ],
+        "Jihočeský kraj": [
+            "České Budějovice", "Český Krumlov", "Jindřichův Hradec", "Písek", "Prachatice", "Strakonice", "Tábor"
+        ],
+        "Jihomoravský kraj": [
+            "Blansko", "Brno-město", "Brno-venkov", "Břeclav", "Hodonín", "Vyškov", "Znojmo"
+        ],
+        "Karlovarský kraj": [
+            "Cheb", "Karlovy Vary", "Sokolov"
+        ],
+        "Královéhradecký kraj": [
+            "Hradec Králové", "Jičín", "Náchod", "Rychnov nad Kněžnou", "Trutnov"
+        ],
+        "Liberecký kraj": [
+            "Česká Lípa", "Jablonec nad Nisou", "Liberec", "Semily"
+        ],
+        "Moravskoslezský kraj": [
+            "Bruntál", "Frýdek-Místek", "Karviná", "Nový Jičín", "Opava", "Ostrava-město"
+        ],
+        "Olomoucký kraj": [
+            "Jeseník", "Olomouc", "Prostějov", "Přerov", "Šumperk"
+        ],
+        "Pardubický kraj": [
+            "Chrudim", "Pardubice", "Svitavy", "Ústí nad Orlicí"
+        ],
+        "Plzeňský kraj": [
+            "Domažlice", "Klatovy", "Plzeň-město", "Plzeň-jih", "Plzeň-sever", "Rokycany", "Tachov"
+        ],
+        "Středočeský kraj": [
+            "Benešov", "Beroun", "Kladno", "Kolín", "Kutná Hora", "Mělník", "Mladá Boleslav", "Nymburk", "Praha-východ", "Praha-západ", "Příbram", "Rakovník"
+        ],
+        "Ústecký kraj": [
+            "Chomutov", "Děčín", "Litoměřice", "Louny", "Most", "Teplice", "Ústí nad Labem"
+        ],
+        "Kraj Vysočina": [
+            "Havlíčkův Brod", "Jihlava", "Pelhřimov", "Třebíč", "Žďár nad Sázavou"
+        ],
+        "Zlínský kraj": [
+            "Kroměříž", "Uherské Hradiště", "Vsetín", "Zlín"
+        ]
+    }
+}
