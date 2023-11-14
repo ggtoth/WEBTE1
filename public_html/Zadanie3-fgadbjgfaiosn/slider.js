d58d4cf8eaab29622bfa15c3bdff3b9ad5f3d1d3
Gergely Tóth <102431122+GergelyToth-stuba@users.noreply.github.com>
2023-11-14 23:31:13 +0100

    Wtf

--- public_html/Zadanie3-fgadbjgfaiosn/slider.js
+++ public_html/Zadanie3-fgadbjgfaiosn/slider.js
@@ -24,10 +24,19 @@ class RangeSlider extends HTMLElement {
         this.inputTypeRange.step = 0.01;
         this.inputTypeRange.value = this.value;
 
+        let rangeContainer = document.createElement('div');
+        rangeContainer.className = 'range-container';
+
+        this.rangeSpan = document.createElement('span');
+        this.rangeSpan.className = 'custom-range-value';
+        this.rangeSpan.innerText = this.value;
+
         this.container = document.createElement('div');
         this.container.className = 'container';
 
-        this.container.appendChild(this.inputTypeRange);
+        rangeContainer.appendChild(this.inputTypeRange);
+        rangeContainer.appendChild(this.rangeSpan);
+        this.container.appendChild(rangeContainer);
         this.container.appendChild(this.inputTypeNumber);
 
         const style = document.createElement('style');
@@ -37,6 +46,46 @@ class RangeSlider extends HTMLElement {
                     flex-direction: column;
                     width: 10rem;
                  }
+                 
+                .custom-range {
+                  -webkit-appearance: none; /* Override default CSS styles */
+                  appearance: none;
+                  width: 100%; /* Full-width */
+                  height: 15px; /* Specified height */
+                  background: #d3d3d3; /* Grey background */
+                  outline: none; /* Remove outline */
+                  opacity: 1;
+                  margin: 10px 0px;
+                }
+                
+                .custom-range::-webkit-slider-thumb {
+                  -webkit-appearance: none; /* Override default look */
+                  appearance: none;
+                  width: 50px; /* Set a specific slider handle width */
+                  height: 25px; /* Slider handle height */
+                  border-radius: 5px;
+                  background: grey; /* Green background */
+                  cursor: pointer;
+                }
+                
+                .custom-range::-moz-range-thumb {
+                  width: 25px; /* Set a specific slider handle width */
+                  height: 25px; /* Slider handle height */
+                  background: grey; /* Green background */
+                  cursor: pointer;
+                }
+                
+                .range-container{
+                    position: relative;
+                    width: 100%;
+                }
+                
+                .custom-range-value {
+                  position: absolute;
+                  top: 50%;
+                  left: 50%;
+                  transform: translate(-50%, -50%);
+                }
         `;
 
         this.shadowRoot.appendChild(this.container);
@@ -64,6 +113,8 @@ class RangeSlider extends HTMLElement {
     update(){
         this.inputTypeRange.value = this.value;
         this.inputTypeNumber.value = this.value;
+        this.rangeSpan.innerText = this.value;
+
         this.broadcast();
     }
 
