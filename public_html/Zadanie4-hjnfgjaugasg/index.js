2fdb36a2050b929bf30f1f3239105f94190dcc14
Gergely Tóth <102431122+GergelyToth-stuba@users.noreply.github.com>
2023-11-27 21:06:14 +0100

    Moved complicated javascript into html

--- public_html/Zadanie4-hjnfgjaugasg/index.js
+++ public_html/Zadanie4-hjnfgjaugasg/index.js
@@ -29,70 +29,45 @@ function getElementByRelativePath(relativePath) {
     return foundElement || null;
 }
 
-function handleImageClick(event){
-    Array.from(document.body.children).forEach(item => item.classList.add('blur'));
-
-    let modalContainer = document.createElement('div');
-    modalContainer.classList.add('modal');
-    modalContainer.id = 'modal';
-
-    let imageData = getElementByRelativePath(event.currentTarget.getAttribute('src'));
-    let image = document.createElement('img');
-    image.src = imageData.relativePath;
-    image.alt = imageData.description;
+function loadModal(modal, imageData){
+    let image = document.getElementById('modal-image');
+    image.setAttribute('src', imageData.relativePath);
+    image.setAttribute('alt', imageData.description);
     image.classList.add('modal-image');
 
-    let title = document.createElement('h2');
+    let title = document.getElementById('modal-title');
     title.innerText = imageData.title;
 
-    let description = document.createElement('p');
+    let description = document.getElementById('modal-description');
     description.innerText = imageData.description;
 
-    let date = document.createElement('p');
+    let date = document.getElementById('modal-date');
     let dateVar = new Date(imageData.date);
     date.innerText = dateVar.toDateString();
 
-    let coords = document.createElement('p');
+    let coords = document.getElementById('modal-coordinates');
     coords.innerText = `${imageData.gpsCoordinates.latitude}°N, ${imageData.gpsCoordinates.longitude}°E`
-
-    let closeButton = document.createElement('button');
-    closeButton.type = 'button';
-    closeButton.value = close;
-    closeButton.addEventListener('click', removeModal);
-
-    modalContainer.appendChild(image);
-    modalContainer.appendChild(title);
-    modalContainer.appendChild(description);
-    modalContainer.appendChild(date);
-    modalContainer.appendChild(coords);
-    modalContainer.appendChild(closeButton);
-    document.body.appendChild(modalContainer);
 }
 
+function showModal(event){
+    let modal = document.getElementById('modal');
+    Array.from(document.body.children).forEach(item => item !== modal && item.classList.add('blur'));
+    let imageData = getElementByRelativePath(event.currentTarget.getAttribute('src'));
+    loadModal(modal, imageData);
+    modal.classList.remove('hidden');
+}
 
 function removeModal(event){
-    event.currentTarget.parentElement.removeChild(event.currentTarget);
     let modal = document.getElementById('modal');
-    Array.from(document.body.children).forEach(item => item.classList.remove('blur'));
-    modal.parentElement.removeChild(modal);
+    Array.from(document.body.children).forEach(item => item !== modal && item.classList.remove('blur'));
+    modal.classList.add('hidden');
 }
 
 function loadGallery(){
-    let root = document.getElementById('appRoot');
-
-    let container = document.createElement('div');
-    container.classList.add('gallery-container');
-    container.id = 'gallery';
-
-    let filter = document.createElement('input');
-    filter.type = 'text';
-    filter.id = 'gallery-filter';
-    filter.placeholder = 'Write Keyword';
-
+    let filter = document.getElementById('gallery-filter');
     filter.addEventListener('input', filterHandle);
 
-    let imageContainer = document.createElement('div');
-    imageContainer.classList.add('images-container')
+    let imageContainer = document.getElementById('images-container');
 
     data.galleryImages.forEach(function(image){
        let imageFrame = document.createElement('div');
@@ -102,15 +77,11 @@ function loadGallery(){
        imageElement.src = image.relativePath;
        imageElement.alt = `${image.title} ${image.description}`;
        imageElement.classList.add('thumbnail-image');
-       imageElement.addEventListener('click', handleImageClick);
+       imageElement.addEventListener('click', showModal);
 
        imageFrame.appendChild(imageElement);
        imageContainer.appendChild(imageFrame);
     });
-
-    container.appendChild(filter);
-    container.appendChild(imageContainer);
-    root.appendChild(container);
 }
 
 let data;
@@ -119,6 +90,9 @@ window.onload = function(){
         .forEach(function (li){
             li.addEventListener('click', handleNavClick);
     });
+
+    let closeButton = document.getElementById('modal-close-button');
+    closeButton.addEventListener('click', removeModal);
 
     fetch('./gallery.json')
         .then(response => response.json())
